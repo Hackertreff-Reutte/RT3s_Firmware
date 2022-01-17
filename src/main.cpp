9840bbb2ed7c307678934db62cd71a2cc8c31e45
UnHold <alex@hold.re>
2022-01-17 18:52:39 +0100

    changed example code so that only status LED blinks and not the whole screen

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,44 @@
 #include <libopencm3/stm32/gpio.h>
 
 
-int main(){
-
-    __asm volatile ("cpsid i");
-    rcc_periph_clock_enable(RCC_GPIOD);
-	gpio_mode_setup(GPIOD, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8);
-
-    int i;
-    while (1) {
-        gpio_toggle(GPIOD, GPIO8);	/* LED on/off */
-		for (i = 0; i < 1000000; i++) {	/* Wait a bit. */
+void delay(){
+    //just do something (busy waiting)
+    for (int i = 0; i < 10000000; i++) {
 			__asm__("nop");
 		}
+}
+
+int main(){
+    //enable clock
+    rcc_periph_clock_enable(RCC_GPIOE);
+
+    //set led gpio as output
+	gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
+    gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
+
+    //disable read an green led
+    gpio_clear(GPIOE, GPIO0);
+    gpio_clear(GPIOE, GPIO1);
+
+    while (1) {
+
+        //green led on
+        gpio_set(GPIOE, GPIO0);	//green led on
+        delay();
+
+        //red led on
+        gpio_clear(GPIOE, GPIO0); //green led off
+        gpio_set(GPIOE, GPIO1);	//red led on
+        delay();
+
+        //both leds on
+        gpio_set(GPIOE, GPIO0);	//green led on 
+        delay();
+
+        //both leds off
+        gpio_clear(GPIOE, GPIO0);	//green led off
+        gpio_clear(GPIOE, GPIO1);	//red led off
+        delay();
+		
     }
 }
