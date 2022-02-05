4dc699eabceaf69af863231b458e95f94669d67c
UnHold <alex@hold.re>
2022-02-05 14:46:27 +0100

    added radio_tool for linux

--- scripts/wrap_fw.py
+++ scripts/wrap_fw.py
@@ -8,6 +8,6 @@ def after_build(source, target, env):
     if(os.name == 'nt'):
         env.Execute(".\\scripts\\radio_tool\\radio_tool.exe --wrap -o .\\.pio\\\\build\\RT3s\\firmware_wrapped.bin -r UV3X0 -s 0x0800C000:.\\.pio\\build\\RT3s\\firmware.bin")
     else:
-        env.Execute("radio_tool --wrap -o ./.pio/build/RT3s/firmware_wrapped.bin -r UV3X0 -s 0x0800C000:./.pio/build/RT3s/firmware.bin")
+        env.Execute("./scripts/radio_tool/radio_tool --wrap -o ./.pio/build/RT3s/firmware_wrapped.bin -r UV3X0 -s 0x0800C000:./.pio/build/RT3s/firmware.bin")
 
 env.AddPostAction("buildprog", after_build)
