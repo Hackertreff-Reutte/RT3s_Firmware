61a2137c08d3d63d5acc8073a186ac31abed4f3d
UnHold <alex@hold.re>
2022-11-06 01:02:16 +0100

    added comments + some defines for clarification

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 #include "usb/mainUSB.h"
 #include "utils/serial.h"
 
+
+#define VTOR_ADDR 0xE000ED08
+#define VECTOR_TABLE_ADDR 0x0800C000
+
 /*
  * Handler in case our application overflows the stack
  */
@@ -32,12 +36,14 @@ static void task2(void *args __attribute__((unused))) {
 
 int main(){
 
-    *(uint32_t*)0xE000ED08 = 0x0800C000;
+    //fixes the vector table address in the VTOR register
+    //bootloader sets it to 0x08000000, but need to be 0x0800C000
+    *(uint32_t*)VTOR_ADDR = VECTOR_TABLE_ADDR;
 
     rcc_clock_setup_pll(&rcc_hse_8mhz_3v3[RCC_CLOCK_3V3_168MHZ]);
 
 
-    //LED
+    //setup the status LEDs of the radio
     rcc_periph_clock_enable(RCC_GPIOE);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO0);
     gpio_mode_setup(GPIOE, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO1);
